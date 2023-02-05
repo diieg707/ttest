c576040594f9a2f6ea2fa0f418ebff6f07ffef10
diieg707 <54504707+diieg707@users.noreply.github.com>
2023-02-05 17:46:44 -0300

    Update ampbr2.js

--- ampbr2.js
+++ ampbr2.js
@@ -1,7 +1,7 @@
 var box = document.getElementsByClassName("img");
 for (var i = 0; i < box.length; i++) {
 box[i].addEventListener("click", function(f){
-     if (f.target.parentElement.title == 'Achou foto e vídeos íntimos da sua namorada com seu ex'){
+     if (f.target.parentElement.title == 'Conheci minha entiada e hoje sou macho dela'){
      window.open("https://videos.amopornobr.com//amopornobr/_definst_/Internacional/smil:18.HDV31921.smil/playlist.m3u8");}
     
     }
