90ae0067d68d2502f15bb0c6ddde50ce1305129f
diieg707 <54504707+diieg707@users.noreply.github.com>
2023-02-05 17:32:37 -0300

    Update ampbr2.js

--- ampbr2.js
+++ ampbr2.js
@@ -1,7 +1,7 @@
 var box = document.getElementsByClassName("img");
 for (var i = 0; i < box.length; i++) {
 box[i].addEventListener("click", function(f){
-     if (f.target.parentElement.title == 'Achou foto e vídeos íntimos da sua namorada com seu ex'){
+     if (f.target.parentElement.title == 'AMOR o pau dele é muito maior que seu'){
      window.open("https://videos.amopornobr.com//amopornobr/_definst_/Internacional/smil:18.HDV31921.smil/playlist.m3u8");}
     
     }
